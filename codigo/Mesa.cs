a77e5c02acbeeb45148e109b7566669cb2e2481b
Diogo Padoveis Antunes <103612298+dpadoveis@users.noreply.github.com>
2024-04-04 20:13:52 -0300

    Update Mesa.cs

--- codigo/Mesa.cs
+++ codigo/Mesa.cs
@@ -9,17 +9,17 @@ namespace ConsoleApp1
     class Mesa
     {
         #region Atributos
-        private int IdMesa { get; set; }
-        private int Capacidade { get; set; }
-        private bool Ocupada { get; set; }
+        private int _idMesa { get; set; }
+        private int _capacidade { get; set; }
+        private bool _ocupada { get; set; }
         #endregion
 
         #region Construtores
         public Mesa(int idMesa, int capacidade)
         {
-            IdMesa = idMesa;
-            Capacidade= capacidade;
-            Ocupada= false;
+            _idMesa = idMesa;
+            _capacidade= capacidade;
+            _ocupada= false;
         }
         #endregion
 
@@ -27,14 +27,14 @@ namespace ConsoleApp1
         //Verifica se a mesa está ocupada, e caso não esteja, o cliente que fez a requisição a ocupa
         public bool estahReserva()
         {
-            if (Ocupada is false)
+            if (_ocupada is false)
             {
-                Ocupada = true;
-                return Ocupada;
+                _ocupada = true;
+                return _ocupada;
             }
             else
             {
-                return Ocupada;
+                return _ocupada;
             }
         }
         #endregion
