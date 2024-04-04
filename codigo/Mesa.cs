9fafdd61db518e15a3cbb142ce8a707f8bd80cbb
Diogo Padoveis Antunes <103612298+dpadoveis@users.noreply.github.com>
2024-04-04 20:08:20 -0300

    Add files via upload

---
+++ codigo/Mesa.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Mesa
+    {
+        #region Atributos
+        private int IdMesa { get; set; }
+        private int Capacidade { get; set; }
+        public bool Ocupada { get; set; }
+        #endregion
+
+        #region Construtores
+        public Mesa(int idMesa, int capacidade)
+        {
+            IdMesa = idMesa;
+            Capacidade= capacidade;
+            Ocupada= false;
+        }
+        #endregion
+
+        #region Métodos
+        //Verifica se a mesa está ocupada, e caso não esteja, o cliente que fez a requisição a ocupa
+        public bool estahReserva()
+        {
+            if (Ocupada is false)
+            {
+                Ocupada = true;
+                return Ocupada;
+            }
+            else
+            {
+                return Ocupada;
+            }
+        }
+        #endregion
+    }
+
+}
+
