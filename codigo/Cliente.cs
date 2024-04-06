badf70f27bdf3618145b2c699979c3944c77ea18
HenriqueCarrara0 <103524747+HenriqueCarrara0@users.noreply.github.com>
2024-04-06 17:09:23 -0300

    Cliente.cs

--- codigo/Cliente.cs
+++ codigo/Cliente.cs
@@ -1,22 +1,32 @@
 using System;
 
+/// <summary>
+/// Representa um cliente do restaurante.
+/// </summary>
 public class Cliente
 {
-    // Atributos
-    private int idCliente;
-    private string nome;
+    private int idCliente; // Identificador único do cliente
+    private string nome; // Nome do cliente
 
-    // Construtor do "Cliente"
+    /// <summary>
+    /// Cria uma instância de Cliente com o ID e nome fornecidos.
+    /// </summary>
+    /// <param name="idCliente">O identificador único do cliente.</param>
+    /// <param name="nome">O nome do cliente.</param>
     public Cliente(int idCliente, string nome)
     {
         this.idCliente = idCliente;
         this.nome = nome;
     }
 
-    // Método para fazer a reserva
+    /// <summary>
+    /// Faz uma reserva para o cliente em uma mesa com a capacidade especificada.
+    /// </summary>
+    /// <param name="capacidade">A mesa para a qual a reserva será feita.</param>
+    /// <returns>A reserva associada ao cliente e à mesa.</returns>
     public Reserva FazerReserva(int capacidade)
     {
-        // Criar uma nova reserva
+        // Crie uma nova reserva associada ao cliente e à capacidade da mesa
         Reserva reserva = new Reserva(this, capacidade);
         return reserva;
     }
