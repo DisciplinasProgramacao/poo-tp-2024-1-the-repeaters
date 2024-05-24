c70d97ae1d420253f9945b306e54f4d9b7210bc5
Thiago Akihiro Ishi Machado <103148702+Arkquives@users.noreply.github.com>
2024-05-24 12:11:01 -0300

    Update Produto.cs

--- codigo/Produto.cs
+++ codigo/Produto.cs
@@ -6,21 +6,18 @@ using System.Collections.Generic;
         public string Nome { get; private set; }
         public double Valor { get; private set; }
         public int Id { get; private set; }
-        public int Quantidade { get; private set; }
 
         /// <summary>
         /// Construtor da classe Produto.
         /// </summary>
         /// <param name="nome">Nome do produto.</param>
         /// <param name="valor">Valor do produto.</param>
-        /// <param name="id">Identificador único do produto.</param>
-        /// <param name="quantidade">Quantidade do produto disponível.</param>
-        public Produto(string nome, double valor, int id, int quantidade)
+        /// <param name="id">Identificador único do produto.</param>        
+        public Produto(string nome, double valor, int id)
         {
             Nome = nome;
             Valor = valor;
             Id = id;
-            Quantidade = quantidade;
         }
     }
 }
