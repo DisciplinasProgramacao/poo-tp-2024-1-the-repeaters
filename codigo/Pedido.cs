9222aa1bd003622e8073686fbb52c5352c99674c
AbnerFM <103612285+AbnerFM@users.noreply.github.com>
2024-05-27 18:54:54 -0300

    Pedido.cs

--- codigo/Pedido.cs
+++ codigo/Pedido.cs
@@ -1,17 +1,17 @@
-using POO3;
+using POO___Abner;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace POO3
+namespace POO___Abner
 {
     public class Pedido
     {
         private List<Produto> itens = new List<Produto>();
         private static double taxaServico = 0.10;
-        private int quantidadeItens;
+
         private bool pedidoAberto;
 
 
@@ -21,7 +21,7 @@ namespace POO3
             {
                 itens.Add(produto);
             }
-            return quantidadeItens++;
+            return itens.Count;
         }
 
         public void FecharPedido(int numeroPessoas)
@@ -29,7 +29,7 @@ namespace POO3
             pedidoAberto = false;
             double a = CalcularTotal();
             Console.WriteLine($"Total: R$ {a:F2}");
-            Console.WriteLine($"Valor por Pessoa: R$ {(a/numeroPessoas):F2}");  
+            Console.WriteLine($"Valor por Pessoa: R$ {(a / numeroPessoas):F2}");
         }
 
         private double CalcularTotal()
@@ -39,7 +39,7 @@ namespace POO3
             {
                 total += item.Valor;
             }
-            total += total * taxaServico; 
+            total += total * taxaServico;
             return total;
         }
 
