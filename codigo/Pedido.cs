4e75790bcc4977e951d89c9fb05f5f9dc90e00de
AbnerFM <103612285+AbnerFM@users.noreply.github.com>
2024-05-28 09:12:53 -0300

    Pedido.cs

--- codigo/Pedido.cs
+++ codigo/Pedido.cs
@@ -1,9 +1,6 @@
 using POO___Abner;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace POO___Abner
 {
@@ -11,10 +8,13 @@ namespace POO___Abner
     {
         private List<Produto> itens = new List<Produto>();
         private static double taxaServico = 0.10;
-
         private bool pedidoAberto;
 
-
+        /// <summary>
+        /// Adiciona um item ao pedido.
+        /// </summary>
+        /// <param name="produto">Produto a ser adicionado ao pedido.</param>
+        /// <returns>O número de itens no pedido após a adição.</returns>
         public int addItem(Produto produto)
         {
             if (produto != null)
@@ -24,14 +24,22 @@ namespace POO___Abner
             return itens.Count;
         }
 
+        /// <summary>
+        /// Fecha o pedido e exibe o total a ser pago e o valor por pessoa.
+        /// </summary>
+        /// <param name="numeroPessoas">Número de pessoas para dividir o valor total.</param>
         public void FecharPedido(int numeroPessoas)
         {
             pedidoAberto = false;
-            double a = CalcularTotal();
-            Console.WriteLine($"Total: R$ {a:F2}");
-            Console.WriteLine($"Valor por Pessoa: R$ {(a / numeroPessoas):F2}");
+            double total = CalcularTotal();
+            Console.WriteLine($"Total: R$ {total:F2}");
+            Console.WriteLine($"Valor por Pessoa: R$ {(total / numeroPessoas):F2}");
         }
 
+        /// <summary>
+        /// Calcula o valor total do pedido incluindo a taxa de serviço.
+        /// </summary>
+        /// <returns>O valor total do pedido.</returns>
         private double CalcularTotal()
         {
             double total = 0;
@@ -43,6 +51,10 @@ namespace POO___Abner
             return total;
         }
 
+        /// <summary>
+        /// Gera um relatório do pedido.
+        /// </summary>
+        /// <param name="idReservaPed">ID da reserva associada ao pedido.</param>
         public void Relatorio(int idReservaPed)
         {
             Console.WriteLine($"Relatório do Pedido da Reserva ID: {idReservaPed}");
@@ -52,9 +64,14 @@ namespace POO___Abner
             }
         }
 
-
+        /// <summary>
+        /// Indica se o pedido está aberto.
+        /// </summary>
         public bool PedidoAberto { get { return pedidoAberto; } }
 
+        /// <summary>
+        /// Obtém os itens do pedido.
+        /// </summary>
         public List<Produto> Itens { get { return itens; } }
     }
 }
